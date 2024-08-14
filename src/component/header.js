63fbddec2aa00fa5f2dd373ed59833d12f1df5a5
Veeramanikandan37 <manikandanveera40@gmail.com>
2024-08-14 12:22:53 +0530

    Removed all files

---
+++ src/component/header.js
@@ -0,0 +1,18 @@
+function Header(){
+    return (
+       <div className="header">
+        <h1>
+            Students Details 
+        </h1>
+        <ul>
+            <li>Home</li>
+            <li>Products</li>
+            <li>Services</li>
+            <li>About Us</li>
+            <li>Contact Us</li>
+        </ul>
+       </div>
+    );
+}
+
+export default Header;
